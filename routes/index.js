1c89618a6c9da0342f413796c1d70ed2900b7240
chrisgom113 <cgomez4@uci.edu>
2023-05-03 16:32:21 -0700

    created router files, completed user routes

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+const apiRoutes = require('./api');
+
+router.use('/api', apiRoutes);
+
+router.use((req, res) => {
+    return res.send('You in the wrong place, fool!')
+});
+
+module.exports = router;
