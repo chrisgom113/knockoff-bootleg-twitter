d6a3eb47b4fe43f4e766e81592727c8cf6fd48db
chrisgom113 <cgomez4@uci.edu>
2023-05-03 17:18:15 -0700

    removed redundant mogoose properties

--- config/connection.js
+++ config/connection.js
@@ -3,10 +3,8 @@ const mongoose = require("mongoose");
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost/knockoffbootlegtwitter",
   {
-    useFindAndModify: false,
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true,
   }
 );
 
